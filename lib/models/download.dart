7300a5c21442801fad9205a39f1dc0cc703e4265
wassimhamdi2 <whamdi617@gmail.com>
2023-05-10 08:51:44 +0100

    add

--- lib/models/download.dart
+++ lib/models/download.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:flutter/material.dart';
 import 'package:learn/models/user.dart';
@@ -11,7 +10,6 @@ import '../screens/Home.dart';
 import '../screens/profile_screen.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:http/http.dart' as http;
 import 'dart:io';
 
 class Download extends StatefulWidget {
@@ -118,40 +116,6 @@ class _DownloadState extends State<Download> {
           );
         });
   }
-
-  downloadFileFromFirebase(BuildContext context, String downloadUrl) async {
-    // Show a Snackbar to indicate that the download has started.
-    // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    //         content: Text("Downloading file...")));
-
-    try {
-      // Get the filename from the download URL.
-      final Uri uri = Uri.parse(downloadUrl);
-      final String filename = uri.pathSegments.last;
-
-      // Get the local app directory to store the downloaded file.
-      final Directory appDirectory = await getApplicationDocumentsDirectory();
-      final String filePath = '${appDirectory.path}/$filename';
-
-      // Download the file.
-      final http.Response response = await http.get(Uri.parse(downloadUrl));
-      await File(filePath).writeAsBytes(response.bodyBytes);
-
-      // Show a Snackbar to indicate that the download has finished.
-      // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      //   content: Text('File downloaded.'),
-      //   duration: Duration(seconds: 2),
-      // ));
-    } on PlatformException catch (e) {
-      debugPrint("${e}}");
-      // Show an error Snackbar if there was an error downloading the file.
-      // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      //   content: Text('Error downloading file: ${e.message}'),
-      //   duration: Duration(seconds: 2),
-      // ));
-    }
-  }
-
   builDownloadPost() {
     return Container(
       child: Row(
@@ -166,56 +130,55 @@ class _DownloadState extends State<Download> {
           ),
           IconButton(
               onPressed: () async {
-                final scaffoldMessenger = ScaffoldMessenger.of(context);
-                final snackBar = SnackBar(content: Text('Downloading file...'));
-                scaffoldMessenger.showSnackBar(snackBar);
+                 try {
+    // Show a message to indicate that the download has started
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Downloading file from $file...'),
+        duration: Duration(seconds: 1),
+      ),
+    );
 
-                // Get the app's temporary directory to store the downloaded file
-                final tempDir = await getExternalStorageDirectory();
-                final filePath = '${tempDir!.path}/$file';
-                
+    // Use Dio to download the file and save it to the app's external storage directory
+    final dio = Dio();
+    final response = await dio.get(
+      fileUrl,
+      options: Options(responseType: ResponseType.bytes),
+    );
+    final dir = await getExternalStorageDirectory();
+    final filee = File('${dir!.path}/$file');
+    await filee.writeAsBytes(response.data, flush: true);
 
-                try {
-                  // Use Dio to download the file and save it to the app's temporary directory
-                  final dio = Dio();
-                  await dio.download(fileUrl, filePath);
-                  debugPrint("$filePath");
-                  // Show a SnackBar to indicate that the download has finished
-                  final snackBar = SnackBar(content: Text('File downloaded!'));
-                  scaffoldMessenger.showSnackBar(snackBar);
-                } on DioError catch (e) {
-                  // Show a SnackBar with the error message if the download fails
-                  final snackBar =
-                      SnackBar(content: Text('Error: ${e.message}'));
-                  scaffoldMessenger.showSnackBar(snackBar);
-                } catch (e) {
-                  // Show a SnackBar with a generic error message if the download fails for another reason
-                  final snackBar =
-                      SnackBar(content: Text('Error downloading file.'));
-                  scaffoldMessenger.showSnackBar(snackBar);
-                }
+    // Show a message to indicate that the download has finished
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('File downloaded to ${filee.path}.'),
+        duration: Duration(seconds: 1),
+      ),
+    );
+
+  } on DioError catch (e) {
+    // Show a SnackBar with the error message if the download fails
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Error: ${e.message}'),
+        duration: Duration(seconds: 1),
+      ),
+    );
+
+  } catch (e) {
+    // Show a SnackBar with a generic error message if the download fails for another reason
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Error downloading file.'),
+        duration: Duration(seconds: 1),
+      ),
+    );
+
+
+  }
               },
-              // Show a SnackBar to indicate that the download has started
-//   final scaffoldMessenger = ScaffoldMessenger.of(context);
-//   final snackBar = SnackBar(content: Text('Downloading file...'));
-//   scaffoldMessenger.showSnackBar(snackBar);
-//   final String fileName = fileUrl.split('/').last.split('?').first;
-//   final Reference storageRef = FirebaseStorage.instance.refFromURL(fileUrl);
-
-//   final Directory appDocDir = await getApplicationDocumentsDirectory();
-//   final String filePath = '${appDocDir.path}/$fileName';
-//   final File localFile = File(filePath);
-
-//   if (await localFile.exists()) {
-//     // if file already exists locally, return it
-// final snackBar = SnackBar(content: Text('file exist already'));
-//   scaffoldMessenger.showSnackBar(snackBar);
-//   } else {
-//     // if file doesn't exist locally, download it
-//      await storageRef.writeToFile(localFile);
-// final snackBar = SnackBar(content: Text('file downloding'));
-//   scaffoldMessenger.showSnackBar(snackBar);
-//   }
+               
 
               icon: Icon(Icons.download))
         ],
